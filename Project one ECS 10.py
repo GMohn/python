0a41d8990f21352b52a876490d516bbce2b3ea8d
Oracity <gmohn415@gmail.com>
2015-01-15 20:23:00 -0800

    Restaurant Bill

---
+++ Project one ECS 10.py
@@ -0,0 +1,42 @@
+#
+# Name: Geoffrey Mohn
+# ID : 912568148
+# Date: 1/13/15
+#
+import math
+Dollars = int(input('Enter subtotal dollars> '))
+Cents = int(input('Enter subtotal cents  >'))
+Dollarcents = Dollars *100 #converting dollars into cents
+Subtotal = (Dollarcents + Cents) /100 #Converting Dollars and Cents into one value
+
+print('Subtotal: ', '$',Subtotal, sep='') #Print the two numbers next to each other
+
+tax8 = '%.2f' %((Subtotal) * .08 ) #Multiplying for tax value
+tip10 = '%.2f' %((Subtotal) * .1)
+tip15 = '%.2f' %((Subtotal) * .15)
+tip20 = '%.2f' %((Subtotal) * .2)
+
+print('Tax     :', '$',tax8, sep='')
+print('Total   :','%.2f' % (Subtotal *1.08), sep='')
+print('Tip 10%:', '$',tip10, sep='')
+print('Tip 15%:', '$',tip15, sep='')
+print('Tip 20%:', '$',tip20, sep='')
+
+PayDollar = int(input('Enter payment dollars>' ))
+Paycents = int(input('Enter payment cents>' ))
+Convertdollar = PayDollar *100
+Total = (Convertdollar + Paycents)/100 #convert payment into one value
+change = Total - Subtotal
+print(change,'printing change') #unneeded code
+Changelist = [100, 50, 20 , 10 , 5, 1, .25 , .10, .05, .01] #list for change
+Namelist = ['$100 Bill(s):', '$50 Bill(s):','$20 Bill(s):', '$10 Bill(s)','$5 Bill(s):','$1 Bill(s):', 'Quarter(s):','Dime(s):', 'Nickel(s):', 'Penny(ies):']
+amount = []
+for money in Changelist:
+            holdingchange = change
+            change=change//money
+            amount.append(change)
+            change = holdingchange%money
+            
+for i in range(len(Changelist)):
+    print( Namelist[i],amount[i])
+
---
+++ Sphere.py
@@ -0,0 +1,12 @@
+#
+# Name: Geoffrey Mohn
+# ID : 912568148
+# Date: 1/13/15
+#
+import math
+x= int(input('Enter X> '))
+y= int(input('Enter Y> '))
+z= int(input('Enter Z> '))
+point1 = (x, y, z)
+
+
