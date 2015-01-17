589adb00de6a1cb2fe33cd2f79bc97ef29a77a6b
Oracity <gmohn415@gmail.com>
2015-01-16 16:10:29 -0800

    Update Project one ECS 10.py

--- Project one ECS 10.py
+++ Project one ECS 10.py
@@ -12,9 +12,9 @@ Subtotal = (Dollarcents + Cents) /100 #Converting Dollars and Cents into one val
 print('Subtotal: ', '$',Subtotal, sep='') #Print the two numbers next to each other
 
 tax8 = '%.2f' %((Subtotal) * .08 ) #Multiplying for tax value
-tip10 = '%.2f' %((Subtotal) * .1)
+tip10 = '%.2f' %((Subtotal) * .10)
 tip15 = '%.2f' %((Subtotal) * .15)
-tip20 = '%.2f' %((Subtotal) * .2)
+tip20 = '%.2f' %((Subtotal) * .20)
 
 print('Tax     :', '$',tax8, sep='')
 print('Total   :','%.2f' % (Subtotal *1.08), sep='')
@@ -26,10 +26,9 @@ PayDollar = int(input('Enter payment dollars>' ))
 Paycents = int(input('Enter payment cents>' ))
 Convertdollar = PayDollar *100
 Total = (Convertdollar + Paycents)/100 #convert payment into one value
-change = Total - Subtotal
-print(change,'printing change') #unneeded code
+change = Total - (Subtotal *1.08) 
 Changelist = [100, 50, 20 , 10 , 5, 1, .25 , .10, .05, .01] #list for change
-Namelist = ['$100 Bill(s):', '$50 Bill(s):','$20 Bill(s):', '$10 Bill(s)','$5 Bill(s):','$1 Bill(s):', 'Quarter(s):','Dime(s):', 'Nickel(s):', 'Penny(ies):']
+Namelist = ['$100 Bill(s):', ' $50 Bill(s):',' $20 Bill(s):', ' $10 Bill(s):','  $5 Bill(s):','  $1 Bill(s):', '  Quarter(s):','     Dime(s):', '   Nickel(s):', '  Penny(ies):']
 amount = []
 for money in Changelist:
             holdingchange = change
@@ -38,5 +37,5 @@ for money in Changelist:
             change = holdingchange%money
             
 for i in range(len(Changelist)):
-    print( Namelist[i],amount[i])
+    print( Namelist[i],int(amount[i]))
 
