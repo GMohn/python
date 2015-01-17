297c9ce7b93e5f928afc2b0a84f81d6eb3db9c23
Oracity <gmohn415@gmail.com>
2015-01-16 16:10:55 -0800

    Update Sphere.py

--- Sphere.py
+++ Sphere.py
@@ -4,9 +4,21 @@
 # Date: 1/13/15
 #
 import math
-x= int(input('Enter X> '))
-y= int(input('Enter Y> '))
-z= int(input('Enter Z> '))
+x= float(input('Enter X> '))
+y= float(input('Enter Y> '))
+z= float(input('Enter Z> '))
 point1 = (x, y, z)
+origin = (0,0,0)
+Distancex = point1[0] - origin[0]
+Distancey = point1[1] - origin[1]
+Distancez = point1[2] - origin[2]
+Distancesquared = math.sqrt(Distancex * Distancex + Distancey**2 + Distancez**2) #plugging in distance formula
+print('Distance to', point1, ':',' ','%.3f' % (Distancesquared), sep='') #limit to 3 decimal places made sure to add a space between value and colon
+Volume = 4/3*math.pi*Distancesquared**3 #plugging in Volume
+print('Volume of sphere:', '%.3f' % Volume)
+Area = 4*math.pi*Distancesquared**2 #plugging in Area
+print('Area of sphere:', '%.3f' % Area)
+
+
 
 
